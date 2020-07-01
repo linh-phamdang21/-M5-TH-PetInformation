7536d79e73eb99b4d9f027ef3e843436d3022843
linh-phamdang21 <ph.danglinh214@gmail.com>
2020-07-01 22:38:13 +0700

    Init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'M5-TH-PetInformation';
+  petName = 'puppie';
+  petImage = 'http://yourdost-blog-images.s3-ap-southeast-1.amazonaws.com/wp-content/uploads/2016/01/03165939/Dogs.jpg';
+  constructor() { }
+
+  // tslint:disable-next-line:typedef
+  updateName(name) {
+    this.petName = name;
+  }
+
+  // tslint:disable-next-line:typedef
+  updateImage(image) {
+    this.petImage = image;
+  }
 }
